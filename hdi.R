def94b54cf304599a1198e446d7066a576c14902
Shanshan Wu <100401155@alumnos.uc3m.es>
2019-03-13 16:16:56 +0100

    html problems occurs during merging

--- hdi.R
+++ hdi.R
@@ -1,4 +1,3 @@
-
 library(openxlsx)
 library(tidyverse)
 
@@ -46,11 +45,7 @@ hdi.databank.m$Region=rep(".",nrow(hdi.databank.m))
 
 
 ### East Asia and Pacific
-<<<<<<< HEAD
 hdi.databank.m[which(hdi.databank.m$country_name %in% c("Australia","Brunei Darussalam","Cambodia","China","Fiji",
-=======
-hdi.hdi.databank.mbank.m[which(hdi.databank.m$country_name %in% c("Australia","Brunei Darussalam","Cambodia","China","Fiji",
->>>>>>> fc063fbfe2d3920a372af746ef08767e7f7f385f
                                     "Hong Kong, China (SAR)","Indonesia","Japan","Kiribati",
                                     "Korea (Republic of)", "Lao People's Democratic Republic",
                                     "Malaysia","Marshall Islands","Myanmar","Micronesia (Federated States of)","Mongolia",
@@ -78,15 +73,9 @@ hdi.databank.m[which(hdi.databank.m$country_name %in% c("Antigua and Barbuda","A
 hdi.databank.m[which(hdi.databank.m$country_name %in% c("Canada","United States")),]$Region="North America"
 
 
-<<<<<<< HEAD
 ### Africa
 hdi.databank.m[which(hdi.databank.m$country_name %in% c ("Angola","Benin","Botswana","Burkina Faso","Burundi","Cabo Verde","Cameroon","Chad","Central African Republic",
                                      "Eswatini (Kingdom of)","Côte d'Ivoire","Comoros","Congo (Democratic Republic of the)","Congo","Equatorial Guinea","Eritrea","Ethiopia","Gabon","Gambia","Swaziland",
-=======
-### Sub-Saharan Africa
-hdi.databank.m[which(hdi.databank.m$country_name %in% c ("Angola","Benin","Botswana","Burkina Faso","Burundi","Cabo Verde","Cameroon","Chad","Central African Republic",
-                                     "Comoros","Congo (Democratic Republic of the)","Congo","Equatorial Guinea","Eritrea","Ethiopia","Gabon","Gambia","Swaziland",
->>>>>>> fc063fbfe2d3920a372af746ef08767e7f7f385f
                                      "Ghana","Papua New Guinea","Guinea-Bissau","Kenya","Lesotho","Liberia","Madagascar","Malawi","Mali","Mauritania",
                                      "Mauritius","Mozambique","Namibia","Niger","Nigeria","Rwanda","Sao Tome and Principe","Senegal","Seychelles","Guinea",
                                      "Sierra Leone","South Africa","South Sudan","Sudan","Tanzania (United Republic of)","Togo","Uganda","Zambia","Zimbabwe")),]$Region="Africa"
