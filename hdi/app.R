05a2846a4bca20b6cfbfd4a5f3dbbe39970328a7
Shanshan Wu <100401155@alumnos.uc3m.es>
2019-03-29 17:04:51 +0100

    app

--- hdi/app.R
+++ hdi/app.R
@@ -117,6 +117,7 @@ ui <- fluidPage(
                                             uiOutput("heal.ui")
                             ))
                             
+                          
                             
                ),#sidebarPanel
                mainPanel(fluidRow(
@@ -317,4 +318,5 @@ server <- function(input, output,session) {
 
 
 # Run the application 
+
 shinyApp(ui = ui, server = server)
